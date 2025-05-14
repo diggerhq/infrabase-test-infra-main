1a8c81eb9321af03eaa3fe92801fa9930021182e
Igor Zalutski <izalutski@gmail.com>
2025-05-14 16:53:42 -0700

    Create main.tf

---
+++ another/main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" {
+  bucket = "${var.project_name}-${var.environment}-bucket"
+  tags   = local.common_tags
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
